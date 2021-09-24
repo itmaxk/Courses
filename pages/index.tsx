8815f0bb200b1bf4b0af0523e2e25517c18c6f68
Kurgansky Maxim <it.maxk@gmail.com>
2021-09-24 08:42:16 +0300

    Buttons v1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
-import { Htag } from "../components";
+import { Button, Htag } from "../components";
 
 export default function Home(): JSX.Element {
 
   return (
     <>
       <Htag tag='h1'>Текст</Htag>
+      <Button appearance='primary' className="buttonTestClass">Кнопка</Button>
+      <Button appearance='ghost'>Кнопка</Button>
     </>
   );
 }
